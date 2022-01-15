85749555eb81beaab45bd7a1ed73464589235a69
adityogi <73770862+adityogi@users.noreply.github.com>
2022-01-15 15:38:51 +0530

    Updates

--- Sustainability/Sustainibility/layout/scripts/jquery.backtotop.js
+++ Sustainability/Sustainibility/layout/scripts/jquery.backtotop.js
@@ -1,10 +1,10 @@
 /*
 Template Name: Sustainibility
-Author: <a href="https://www.os-templates.com/">OS Templates</a>
-Author URI: https://www.os-templates.com/
+Author: <a href="#">OS Templates</a>
+Author URI: #
 Copyright: OS-Templates.com
 Licence: Free to use under our free template licence terms
-Licence URI: https://www.os-templates.com/template-terms
+Licence URI: #template-terms
 File: Back to Top JS
 */
 
